f93771bab5886a72692191a9fade1c46f94d57d9
dgbailey <bailey.dust@gmail.com>
2019-03-05 15:52:47 -0800

    MVP for DOM II

--- js/index.js
+++ js/index.js
@@ -39,4 +39,65 @@ chatClose.addEventListener('click',function(){
 
 })
 
+//drag
+function dragstart_handler(ev){
+    ev.dataTransfer.setData('text/html', ev.target.id)
+}
+
+function dragover_handler(ev) {
+    ev.preventDefault();
+    // Set the dropEffect to move
+    ev.dataTransfer.dropEffect = "move"
+   }
+function drop_handler(ev) {
+ev.preventDefault();
+// Get the id of the target and add the moved element to the target's DOM
+const data = ev.dataTransfer.getData("text/html");
+console.log(data.type);
+ev.target.appendChild(document.getElementById(document.createElement(data)));
+}
+
+
+//select
+
+function logSelection(event) {
+    const log = document.getElementById('log');
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    log.textContent = `Q: ${selection}`;
+  }
+
+const destination = document.querySelectorAll('input');
+destination.forEach(currentValue => currentValue.addEventListener('select', logSelection));
+
+
+//before input
+
+const input = document.querySelector('input');
+
+input.addEventListener('beforeinput', logKey);
+
+function logKey(e) {
+    let para = document.getElementById('nottyping');
+    
+    if (document.querySelector('input').value.length > 0){
+        para.id = 'typing';
+    }
+    else{
+        typing.id= 'nottyping';
+    }
+
+   
+}
+
+//doubleclick scale
+const main = document.querySelector('#main');
+main.addEventListener('dblclick',function(e){
+    console.log('you clicked and it propped')
+})
+
+const next = document.querySelector('#prop');
+next.addEventListener('dblclick',function(e){
+    console.log('you clicked and it propped');
+    e.stopPropagation();
+})
 
