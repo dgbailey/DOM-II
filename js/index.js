c74d9b7fdf21a257984bc81221737ed36e1e8ade
dgbailey <bailey.dust@gmail.com>
2019-03-05 13:42:20 -0800

    chat bot with 4 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-// Your code goes here
+// scroll
+window.onscroll = function() {myFunction()};
+      
+function myFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    const images = document.querySelectorAll(".img-content");
+    images.forEach(currentValue => currentValue.id = "slideUp");
+}
+if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    const images = document.querySelectorAll(".destination");
+    images.forEach(currentValue => currentValue.id = "slideUp");
+}
+}
+
+//mouseover
+const travelBtn = document.querySelectorAll('.btn');
+
+
+function popUp(domItem){
+    domItem.forEach(currentValue => currentValue.addEventListener('mouseover',function(){
+        const poppy = document.querySelector('.chat-window');
+        poppy.id = "chat";
+    }));
+
+    // domItem.forEach(currentValue => currentValue.addEventListener('mouseout',function(){
+    //     const nopoppy = document.querySelector('.chat-window');
+    //     nopoppy.id = "";
+    // }));
+}
+
+
+//click
+popUp(travelBtn);
+
+const chatClose = document.querySelector('.close');
+chatClose.addEventListener('click',function(){
+    const nopoppy = document.querySelector('.chat-window');
+    nopoppy.id = "";
+
+})
+
+
